93fd5197a940445762096b5db1cd3c3fd3a5dd3d
basant7 <basantchoudhary135@gmail.com>
2024-06-19 18:17:13 +0530

    added comments

--- Array/array.js
+++ Array/array.js
@@ -1,19 +1,25 @@
 // second largest
 
-let array = [12, 10, 8, 15, 14, 9]
+let array = [12, 10, 8, 15, 14, 9];
 
-function secondLargest(arr){
-    let max = -Infinity, secondMax = -Infinity;
-    for(let i = 0; i< arr.length; i++){
-        if(max < arr[i]){
-            secondMax = max;
-            max = arr[i];
-        }else if(secondMax < arr[i] && max != arr[i]){
-            secondMax = arr[i]
-        }
+function secondLargest(arr) {
+  // give max the predefault lowest value
+  let max = -Infinity,
+    secondMax = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    // check if max is less than the current element then
+    //  replace the value of max with the current element
+    if (max < arr[i]) {
+      secondMax = max;
+      max = arr[i];
+      //   for the secondMax check if the current element is greater than the secondMax
+      //  and also check if the current element is not the same as the max
+    } else if (secondMax < arr[i] && max != arr[i]) {
+      secondMax = arr[i];
     }
-    console.log("largest element in the array is ", max)
-    console.log("Second largest element in the array is ", secondMax)
+  }
+  console.log("largest element in the array is ", max);
+  console.log("Second largest element in the array is ", secondMax);
 }
 
-secondLargest(array)
+secondLargest(array);
