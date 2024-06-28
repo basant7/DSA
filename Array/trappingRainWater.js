e710133de6332048722e7b316882e8867ec10cd0
basant7 <basantchoudhary135@gmail.com>
2024-06-28 07:45:03 +0530

    trapping rain water problem comments

--- Array/trappingRainWater.js
+++ Array/trappingRainWater.js
@@ -4,7 +4,12 @@ function trappingRainWater(arr) {
   let leftMax = [],
     rightMax = [],
     sum = 0;
+  // we create twp arrays and fill each array with max elements
   leftMax[0] = arr[0];
+  //   fill the first array with the elements given that
+  //  if the new array previous value is greater than the original array current value
+  // then set the new value in the new array as previous value of new array
+  //  else set the new array value as the current array value
   for (let i = 1; i < arr.length; i++) {
     if (leftMax[i - 1] > arr[i]) {
       leftMax[i] = leftMax[i - 1];
@@ -12,6 +17,7 @@ function trappingRainWater(arr) {
       leftMax[i] = arr[i];
     }
   }
+  //   start from the last element and do same as above with other new array
   rightMax[arr.length - 1] = arr[arr.length - 1];
   for (let i = arr.length - 2; i >= 0; i--) {
     if (rightMax[i + 1] > arr[i]) {
@@ -20,6 +26,8 @@ function trappingRainWater(arr) {
       rightMax[i] = arr[i];
     }
   }
+  //   now check for the smaller value from both new arrays and
+  //  add subtract it with the original array value and add it to the sum
   for (let i = 1; i < arr.length - 1; i++) {
     if (leftMax[i] < rightMax[i]) {
       sum += leftMax[i] - arr[i];
