897751c3cf21e8bd342e6ba8aa2d1ced72276636
basant7 <basantchoudhary135@gmail.com>
2024-07-01 08:03:58 +0530

    max subarray comments

--- Array/maxSumSubArray.js
+++ Array/maxSumSubArray.js
@@ -5,11 +5,16 @@ function maxSumSubArray(arr) {
   let max = arr[0];
   let current = arr[0];
   while (i < arr.length) {
+    // check if the current element + previous element is less than the current element then
+    // add the current element to current variable
     if (current + arr[i] < arr[i]) {
       current = arr[i];
     } else {
+      // if the current variable + previous element is greater than the current element then
+      // take current variable + previous element as the new current variable
       current += arr[i];
     }
+    // compare current and max and update max
     max = current > max ? current : max;
     i++;
   }
