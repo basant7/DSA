e4b64965d69ea94024b944305a81725c5fa9b917
basant7 <basantchoudhary135@gmail.com>
2024-07-03 11:48:01 +0530

    circular max sum comments

--- Array/circularSum.js
+++ Array/circularSum.js
@@ -1,5 +1,6 @@
 let array = [8, -4, 3, -5, 4];
 
+// find the max sum of subarray
 function normalSum(arr) {
   let normalSum = arr[0];
   let currentSum = arr[0];
@@ -15,20 +16,26 @@ function normalSum(arr) {
   return normalSum;
 }
 
-// console.log(normalSum(array));
-
+// check the largest circular sum
 function circularSum(arr) {
   let maxNormal = normalSum(arr);
+  // if the maximum sum of the subarray is less than 0 then return the sum
+  // because all the elements will be -ve then
   if (maxNormal < 0) {
     return maxNormal;
   }
   let max_sum = 0;
+  // loop through the array
   for (let i = 0; i < arr.length; i++) {
+    // add all the elements of the array and add it to the max_sum
     max_sum += arr[i];
+    // make -ve elements +ve and vice versa
     arr[i] = -arr[i];
   }
+  // now add the max_sum with the maximum sum of subarray of the reversed array
   let maxCircularSum = max_sum + normalSum(arr);
+  // now check for the max value and return it
   return maxNormal > maxCircularSum ? maxNormal : maxCircularSum;
 }
 
-console.log(circularSum(array))
+console.log(circularSum(array));
