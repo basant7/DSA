1892e1f642b8ecaf8beae4bfc080d1525e45aadc
basant7 <basantchoudhary135@gmail.com>
2024-07-08 07:55:32 +0530

    added comments

--- Array/subArrayWithGivenSum.js
+++ Array/subArrayWithGivenSum.js
@@ -5,9 +5,13 @@ function subArrayWithGivenSum(arr, k) {
   let curr = 0;
   let j = 0;
   for (let i = 0; i < arr.length; i++) {
+    // check if the current sum is < k then add the ith element to the current sum
     if (curr < k) {
       curr = curr + arr[i];
     }
+    // if the current sum becomes greater than the k
+    // remove the element at j from the current sum and then increment the j to next position
+    // also decrement the i to reloop the current element
     if (curr > k) {
       curr = curr - arr[j];
       i--;
@@ -21,16 +25,19 @@ function subArrayWithGivenSum(arr, k) {
 }
 
 // geeks of geeks solution
-function sumArray(arr, sum) {
+function sumArray(arr, k) {
   let curr = 0;
   let j = 0;
   for (let i = 0; i < arr.length; i++) {
     curr = curr + arr[i];
-    while (sum < curr) {
+    // check if k is < current sum then minus the element at index j from the current sum
+    // and increment the j
+    // run loop and do the above until k becomes equal or greater than current sum
+    while (k < curr) {
       curr -= arr[j];
       j++;
     }
-    if (curr == sum) {
+    if (curr == k) {
       return "yes";
     }
   }
