9bd0b72df0776a8372f655594190e1f3b2258ab1
basant7 <basantchoudhary135@gmail.com>
2024-07-09 09:31:44 +0530

    max appear rework better solution

--- Array/maxAppear.js
+++ Array/maxAppear.js
@@ -18,4 +18,23 @@ function maxAppear(arr1, arr2) {
   return res;
 }
 
+function maxAppearBetter(leftArray, rightArray) {
+  let freq = new Array(101).fill(0);
+  // Traverse through all ranges and mark begining and ending in freq array
+  // eq if 1st element in left array is 2 then at 2nd position the value will increment by 1
+  // and if the 1st element of the right array is 4 then the 4+1=5th position will be decremented
+  for (let i = 0; i < leftArray.length; i++) {
+    freq[leftArray[i]]++;
+    freq[rightArray[i] + 1]--;
+  }
+  let res = 0;
+  //   now do prefix in the freq array and check for the largest value there and return its index
+  for (let i = 1; i < freq.length; i++) {
+    freq[i] += freq[i - 1];
+    if (freq[i] > res) res = i;
+  }
+  return res;
+}
+
 console.log(maxAppear(array1, array2));
+console.log(maxAppearBetter(array1, array2));
