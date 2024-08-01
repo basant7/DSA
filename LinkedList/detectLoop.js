134eb5846078a6f92df2085697bda6bd1bd6f51e
basant7 <basantchoudhary135@gmail.com>
2024-08-01 07:59:15 +0530

    detect loop

--- LinkedList/detectLoop.js
+++ LinkedList/detectLoop.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(value) {
+  constructor(value = null) {
     this.value = value;
     this.next = null;
   }
@@ -43,13 +43,45 @@ class LinkedList {
     }
   }
 
-  isLLCircular() {
+  // two pointer approach
+  detectLoop_1() {
+    // take two pointers
+    // 1st pointer will jump 1 step at a time
+    // 2nd pointer will jump 2 steps at a time
     let R1 = this.head;
     let R2 = R1.next;
     while (R2 && R2.next) {
+      // if both the pointers become same
       if (R1 == R2) return true;
+      //   jump 1 step
       R1 = R1.next;
+      //   jump 2 steps
       R2 = R2.next.next;
+    }
+    return false;
+  }
+
+  // point each node to temp and
+  // if any node already points to temp then it is a loop
+  detectLoop_2() {
+    // create a temp node
+    let temp = new Node();
+    let current = this.head;
+    while (current != null) {
+      // if current's next is null then return because its not a loop then
+      if (current.next == null) {
+        return false;
+      }
+      //   if current's next == temp that means it is a loop
+      if (current.next == temp) {
+        return true;
+      }
+      //   store the next element
+      let next = current.next;
+      //   point current to temp
+      current.next = temp;
+      //   update temp
+      current = next;
     }
     return false;
   }
@@ -66,4 +98,5 @@ linkedList.push(60);
 linkedList.push(70);
 linkedList.makeLLCircular();
 //   linkedList.print();
-console.log(linkedList.isLLCircular());
+console.log(linkedList.detectLoop_1());
+console.log(linkedList.detectLoop_2());
