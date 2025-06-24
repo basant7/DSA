9dd9e215fbe818bf70af6bdc8c7d2ba7e2b9bd9d
basant <basantchoudhary135@gmail.com>
2025-06-24 07:14:20 +0530

    first unique character

---
+++ Array/array-basics/LPS_KMP.js
@@ -0,0 +1,22 @@
+let needle = "ababcabc";
+
+function findLPS(needle) {
+  let lps = new Array(needle.length).fill(0);
+  let len = 0;
+  let i = 1;
+  while (i < needle.length) {
+    if (needle[i] === needle[len]) {
+      len++;
+      lps[i] = len;
+      i++;
+    } else if (len > 0) {
+      len = lps[len - 1];
+    } else {
+      lps[i] = 0;
+      i++;
+    }
+  }
+  return lps;
+}
+
+console.log(findLPS(needle));
--- Array/array-basics/firstUniqueCharacter.js
+++ Array/array-basics/firstUniqueCharacter.js
@@ -2,19 +2,28 @@ let s = "aabb";
 var firstUniqChar = function (s) {
   for (let i = 0; i < s.length; i++) {
     let j = 0;
-    let isThere = false;
+    let isFound = false;
     while (j < s.length) {
-      if (i !== j && s[i] != s[j]) {
-        isThere = true;
+      if (i !== j && s[i] == s[j]) {
+        isFound = true;
         break;
       }
       j++;
     }
-    if (!isThere) {
+    if (!isFound) {
       return i;
     }
   }
   return -1;
 };
 
-console.log(firstUniqChar(s)); // Output: 0
+console.log(firstUniqChar(s)); // Output: -1
+
+// todo
+// add question link
+// add time and space complexity
+// add more test cases
+// add explanation of the code
+// add github link
+// add leetcode link
+// add approaches
